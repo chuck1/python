52042f0f7a8eb46e0f810a6d32f6351e6e3fdf47
Charles Rymal <charles.rymal@nortek.com>
2017-04-28 16:57:58 -0700

    auto commit

--- __init__.py
+++ __init__.py
@@ -1,6 +1,33 @@
 
 import os
 
+def bin_compare(b0,b1):
+    for c0,c1 in zip(b,b1):
+        try:
+            s0 = chr(c0)
+            s1 = chr(c1)
+        except Exception as e:
+            print(e)
+            s0 = ''
+            s1 = ''
+        
+        msg = '' if c0==c1 else 'differ'
+        
+        print("{:02x} {:02x} {:6} {:6} {}".format(c0,c1,repr(s0),repr(s1),msg))
+
+def check_existing_binary_data(filename, b0):
+    if os.path.exists(filename):
+        with open(filename, 'rb') as f:
+            b1 = f.read()
+        
+        if b0 == b1:
+            return False
+        else:
+            #bin_compare(b0,b1)
+            return True
+    else:
+        return True
+
 class BuildError(Exception):
     def __init__(self, message):
         super(BuildError, self).__init__(message)
@@ -90,6 +117,7 @@ class Rule(object):
         f_out = list(self.f_out())
         
         should_build, f = self.check(makefile, f_out, f_in, test)
+
         if should_build:
             if test:
                 print('build',f_out,'because',f)
@@ -100,7 +128,20 @@ class Rule(object):
                     raise BuildError(str(self) + ' return code ' + str(ret))
 
         self.up_to_date = True
+    
+    def write_text(self, filename, s):
+        if check_existing_binary_data(filename, s.encode()):
+            with open(filename, 'w') as f:
+                f.write(s)
+        else:
+            print('binary data unchanged. do not write.')
 
+    def write_binary(self, filename, b):
+        if check_existing_binary_data(filename, b):
+            with open(filename, 'wb') as f:
+                f.write(b)
+        else:
+            print('binary data unchanged. do not write.')
 
 """
 a rule to which we can pass a static list of files for f_out and f_in
