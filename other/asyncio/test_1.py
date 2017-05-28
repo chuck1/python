1504fffc855d16fe380e68801c66558ed83bcc1a
charles rymal <charlesrymal@gmail.com>
2017-05-27 23:16:31 -0700

    asyncio and aiohttp

--- other/asyncio/test_1.py
+++ other/asyncio/test_1.py
@@ -1,10 +1,11 @@
-
+import time
 import concurrent.futures
 import asyncio
 
 
 def func(i):
     print('task %i' % i)
+    time.sleep(1)
 
 
 
@@ -13,7 +14,11 @@ loop = asyncio.get_event_loop()
 with concurrent.futures.ProcessPoolExecutor() as executor:
     for i in range(10):
         print(loop.run_in_executor(executor, func, i))
+    print('exiting with statement')
+    # the executor will block here until all threads are finished
 
+print('closing loop')
 loop.close()
+print('loop closed')
 
 
