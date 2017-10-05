af5d04ff302dcf5903003d0f594e51d6fa01b55f
Charles Rymal <charles.rymal@nortek.com>
2017-10-05 10:28:16 -0700

    animate move_tool, move_arms, and gcode

--- projects/computer_navigated_platform/main.py
+++ projects/computer_navigated_platform/main.py
@@ -1,36 +1,39 @@
 import math
+import re
+import functools
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import mpl_toolkits.mplot3d as a3
+import scipy.optimize
 #import mpl_toolkits.mplot3d.art3d
 
 X = np.array([1,0,0])
 Y = np.array([0,1,0])
 Z = np.array([0,0,1])
 
-def solve(X_, p):
-    for i in range(6):
-        p.arms[i].q = X_[4*i:4*i+4]
+def solve1(X_, p):
+    p.tool.o = X_[0:3]
+    p.tool.q = X_[3:7]
     
-    p.tool.o = X_[24:27]
-    p.tool.q = X_[27:31]
-    
-    Y = np.zeros(31)
+    Y = np.zeros(7)
+   
+    Y[0] = np.linalg.norm(p.tool.q) - 1
 
     for i in range(6):
         j = (i-i%2)//2
-
-        y = p.arms[i].p() - p.tool.point(j)
         
-        Y[k:k+3] = y
-        k += 3
+        l = np.linalg.norm(p.arms[i].o - p.tool.point(j))
 
-        y = np.linalg.norm(p.arms[i].q) - 1
-
-        Y[k] = y
-        k += 1
-
+        Y[i + 1] = l - p.arms[i].l
     
+    return Y
+
+def solve(p):
+    x = scipy.optimize.fsolve(solve1, np.concatenate((p.tool.o, p.tool.q)), (p,))
+    p.tool.o = x[0:3]
+    p.tool.q = x[3:7]
+    p.update_arms()
 
 def quaternion_multiply(quaternion1, quaternion0):
     w0, x0, y0, z0 = quaternion0
@@ -77,13 +80,28 @@ class Arm:
     def p(self):
         return self.o + self.l * rotate(X, self.q)
 
-    def plot(self, ax):
-        o = self.o
-        p = self.p()
-        ax.plot(
-                [o[0], p[0]],
-                [o[1], p[1]],
-                [o[2], p[2]])
+    def get_plot_data(self):
+        o = np.reshape(self.o, (3,1))
+        p = np.reshape(self.p(), (3,1))
+        data = np.concatenate((o, p), 1)
+        return data
+
+    def plot(self, ax, ax_2dx, ax_2dy, ax_2dz, linewidth):
+        data = self.get_plot_data()
+        self.line, = ax.plot(data[0], data[1], data[2], linewidth=linewidth)
+        self.line_2dx, = ax_2dx.plot(data[1], data[2], linewidth=linewidth)
+        self.line_2dy, = ax_2dy.plot(data[0], data[2], linewidth=linewidth)
+        self.line_2dz, = ax_2dz.plot(data[0], data[1], linewidth=linewidth)
+
+    def plot_update(self):
+        data = self.get_plot_data()
+
+        self.line.set_data(data[0:2])
+        self.line.set_3d_properties(data[2])
+        
+        self.line_2dx.set_data(data[np.array([1,2])])
+        self.line_2dy.set_data(data[np.array([0,2])])
+        self.line_2dz.set_data(data[np.array([0,1])])
 
 class Tool:
     def __init__(self, l, o, q):
@@ -95,10 +113,49 @@ class Tool:
         v = rotate(rotate(X, axis_angle(Z, math.pi * 2 / 3 * i)), self.q)
         return self.o + self.l * v
 
-class Platform:
-    def __init__(self):
+    def plot_data(self, i):
+        o = np.reshape(self.point(i), (3,1))
+        p = np.reshape(self.point((i+1)%3), (3,1))
+        data = np.concatenate((o, p), 1)
+        return data
 
-        self.tool = Tool(0.1, np.array([0,0,-0.4]), axis_angle(X, math.pi/2))
+    def plot(self, ax, ax_2dx, ax_2dy, ax_2dz):
+        self.lines = []
+        self.lines_2dx = []
+        self.lines_2dy = []
+        self.lines_2dz = []
+
+        for i in range(3):
+            data = self.plot_data(i)
+            
+            line, = ax.plot(data[0], data[1], data[2])
+            self.lines.append(line)
+    
+            line, = ax_2dx.plot(data[1], data[2])
+            self.lines_2dx.append(line)
+
+            line, = ax_2dy.plot(data[0], data[2])
+            self.lines_2dy.append(line)
+
+            line, = ax_2dz.plot(data[0], data[1])
+            self.lines_2dz.append(line)
+
+    def plot_update(self):
+        for i in range(3):
+            data = self.plot_data(i)
+            
+            line = self.lines[i]
+            line.set_data(data[0:2])
+            line.set_3d_properties(data[2])
+
+            self.lines_2dx[i].set_data(data[np.array([1,2])])
+            self.lines_2dy[i].set_data(data[np.array([0,2])])
+            self.lines_2dz[i].set_data(data[np.array([0,1])])
+
+class Platform:
+    def __init__(self, tool_q):
+
+        self.tool = Tool(0.1, np.array([0,0,-0.4]), tool_q)
         
         arm_anchor_radius = 0.5
 
@@ -126,22 +183,71 @@ class Platform:
 
             self.arms.append(arm)
 
-    def plot(self):
-        fig = plt.figure()
-        #ax = fig.add_subplot(111, projection='3d')
-        ax = a3.Axes3D(fig)
-        for arm in self.arms:
-            arm.plot(ax)
-        
-        verts = [[self.tool.point(i) for i in range(3)]]
-        print(verts)
-        tri = a3.art3d.Poly3DCollection(verts)
-        ax.add_collection3d(tri)
-        
-        p = [-.5,.5]
-        ax.plot(p,p,p)
+    def move_tool(self, o, q):
+        self.tool.o = o
+        self.tool.q = q
+        self.update_arms()
     
-p = Platform()
+    def move_arms(self, moves):
+        for i, l in moves:
+            self.arms[i].l = l
+
+        solve(self)
+
+    def update_arms(self):
+        for i in range(6):
+            j = (i-i%2)//2
+
+            o = self.arms[i].o
+            
+            p = self.tool.point(j)
+            v = p - o
+
+            self.arms[i].l = np.linalg.norm(v)
+
+            q = from_vectors(X, v)
+
+            self.arms[i].q = q
+
+    def get_tool_verts(self):
+        return [[self.tool.point(i) for i in range(3)]]
+
+    def plot(self, fig, fig2d):
+        ax = a3.Axes3D(fig)
+        
+        ax_2dx = fig2d.add_subplot(2,2,1)
+        ax_2dy = fig2d.add_subplot(2,2,2)
+        ax_2dz = fig2d.add_subplot(2,2,3)
+
+        for i, arm in zip(range(6), self.arms):
+            linewidth = 3 if i == 0 else 1
+            arm.plot(ax, ax_2dx, ax_2dy, ax_2dz, linewidth)
+       
+        self.tool.plot(ax, ax_2dx, ax_2dy, ax_2dz)
+
+        p = np.array([-0.5,0.5])
+        ax.set_xlim3d(p)
+        ax.set_ylim3d(p)
+        ax.set_zlim3d(p-0.5)
+
+    def plot_update(self):
+        for arm in self.arms:
+            arm.plot_update()
+        
+        self.tool.plot_update()
+
+    def print_(self):
+        print('tool')
+        print('    {}'.format(self.tool.o))
+        print('    {}'.format(self.tool.q))
+        print('arms')
+        for a in self.arms:
+            print('    {}'.format(a.o))
+            print('    {}'.format(a.l))
+
+p = Platform(axis_angle(Z,2*math.pi/12))
+
+#axis_angle(X, math.pi/2)
 
 if False:
     print(rotate(X, axis_angle(Y, math.pi/2)))
@@ -159,8 +265,145 @@ if False:
     print(Z)
     print(rotate(X, q))
 
-p.plot()
+p.print_()
+
+
+p.print_()
+
+if False:
+    p.plot()
+    plt.show()
+
+def circle(i, z):
+    period_in_frames = 60
+    a = math.pi * 2 / period_in_frames * i
+    r = 0.25
+    x = r * math.sin(a)
+    y = r * math.cos(a)
+    z = z
+    return np.array([x, y, z])
+
+fps = 20
+
+AXIS = {'X':0,'Y':1,'Z':2}
+
+def make_path(p, q, n):
+    v = q - p
+    return [p + v * (i+1)/n for i in np.arange(n)]
+
+class Program:
+    def __init__(self, platform, fps):
+        self.platform = platform
+        self.feed_rate = .02
+        self.fps = fps
+        self.lines = []
+        self.frames = []
+
+    def read_line(self):
+        if not self.lines:
+            return None
+
+        s = self.lines.pop(0)
+        
+        print(s)
+        
+        pat = re.compile("G1 ([XYZ])(\d+\.?\d*)")
+        m = pat.match(s)
+        if m:
+            print(m.groups())
+            p = float(m.group(2)) / 1000
+            
+            o1 = np.array(self.platform.tool.o)
+            o2 = np.array(o1)
+            
+            axis = AXIS[m.group(1)]
+
+            o2[axis] = p
+
+            d = np.linalg.norm(o2 - o1)
+            
+            duration = d / self.feed_rate
+            print('d',d)
+            print('fr',self.feed_rate)
+            frames = duration * self.fps
+            
+            path = make_path(o1, o2, frames)
+            
+            new_frames = [functools.partial(self.platform.move_tool, o, self.platform.tool.q) for o in path]
+            
+            print('added {} frames'.format(frames))
+
+            self.frames += new_frames
+
+            return
+
+        raise RuntimeError('invalid gcode command {}'.format(repr(s)))
+
+    def next_frame(self):
+
+        if not self.frames:
+            self.read_line()
+        
+        if not self.frames:
+            return None
+
+        return self.frames.pop(0)
+
+def update_plot_(num, p, prog):
+    #p.move_tool(circle(num, -0.5), p.tool.q)
+    #p.move_arms([(0, p.arms[0].l + 0.001 / fps)])
+    
+    print('frame {}'.format(num))
+    f = prog.next_frame()
+    if f: f()
+
+    #print(' '.join(['{:.8f}'.format(a.l) for a in p.arms]))
+
+    p.plot_update()
+    
+    yield from p.tool.lines
+    
+    for a in p.arms:
+        yield a.line
+
+def update_plot_2d_(num, p, prog):
+    print('frame {}'.format(num))
+    f = prog.next_frame()
+    if f: f()
+
+    p.plot_update()
+
+    yield from p.tool.lines_2dx
+    yield from p.tool.lines_2dy
+    yield from p.tool.lines_2dz
+    
+    for a in p.arms:
+        yield a.line_2dx
+        yield a.line_2dy
+        yield a.line_2dz
+
+def update_plot(num, p, prog):
+    return list(update_plot_(num, p, prog))
+
+def update_plot_2d(num, p, prog):
+    return list(update_plot_2d_(num, p, prog))
+
+fig = plt.figure()
+fig2d = plt.figure()
+
+p.plot(fig, fig2d)
+
+frames = 60
+
+prog = Program(p, fps)
+prog.lines.append("G1 X100")
+
+#line_ani_3d = animation.FuncAnimation(fig, update_plot, frames, fargs=(p, prog), interval=1000/fps, blit=True)
+line_ani_2d = animation.FuncAnimation(fig2d, update_plot_2d, frames, fargs=(p, prog), interval=1000/fps, blit=True)
+
 plt.show()
+
+#print(line_ani)
 
 
 
