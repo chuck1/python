59b221df59a302eb1b117c303252400d5fde4a79
Charles Rymal <charles.rymal@nortek.com>
2018-02-21 14:41:04 -0800

    nodejs ws test

---
+++ node_js/websockets/server.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 13000 });
+
+wss.on('connection', function connection(ws) {
+	
+	console.log("new connection");
+
+	ws.on('message', function incoming(message) {
+		      console.log('received: %s', message);
+		    });
+	
+	ws.on('error', function(e) {
+		console.log('error', e);
+	});
+
+	ws.on('close', function(e) {
+		console.log('close', e);
+	});
+
+	ws.send('something');
+});
+
