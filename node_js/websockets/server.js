f75340d2a4478057a4bf6cba4b887ed8f3810d87
Charles Rymal <charles.rymal@nortek.com>
2018-02-22 16:56:39 -0800

    auto commit

--- node_js/websockets/server.js
+++ node_js/websockets/server.js
@@ -2,6 +2,8 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ host: '0.0.0.0', port: 13000 });
 
+console.log(wss);
+
 wss.on('connection', function connection(ws) {
 	
 	console.log("new connection");
