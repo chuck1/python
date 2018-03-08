aad0fc4448ab4e3421c4c723106cddbd0bf02618
Charles Rymal <charles.rymal@nortek.com>
2018-03-07 16:41:07 -0800

    auto commit

---
+++ projects/encode/main.py
@@ -0,0 +1,21 @@
+
+import struct
+
+def encode1(i):
+    
+    52
+    
+def encode(i):
+    
+    print(i)
+
+    b = struct.pack("L", i)
+
+    print(b)
+
+    for i in b:
+        print(i)
+    
+
+encode(9375758743957974111)
+
