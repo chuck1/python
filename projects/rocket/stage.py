73203a2bf912911f2b39204be879f7a72d3be859
Charles Rymal <charles.rymal@nortek.com>
2018-04-10 16:12:46 -0700

    auto commit

--- projects/rocket/stage.py
+++ projects/rocket/stage.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 class Radio(object):
     def power(self, d, E):
@@ -12,8 +13,13 @@ class Radio(object):
 
         return P
 
-class Rocket(object):
-    stages=list()
+class Rocket:
+    stages = []
+
+    @property
+    def mass_wet(self):
+        return sum([s.m_wet for s in stages])
+
     def deltav(self):
         stages = list(self.stages)
 
@@ -24,46 +30,46 @@ class Rocket(object):
         while stages:
             s = stages.pop(0)
 
-            m_dry = m_wet - s.m_wet + s.m_dry
+            m_dry = m_wet - s.mass_prop
             
-            dv_temp = s.Isp * 9.81 * math.log(m_wet / m_dry)
+            dv_temp = s.isp * 9.81 * math.log(m_wet / m_dry)
 
             dv += dv_temp
 
             if 0:
-                print "m_wet  ", m_wet
-                print "m_dry  ", m_dry
-                print "log    ", math.log(m_wet / m_dry)
-                print "log    ", m_wet / m_dry
-                print "Isp    ", s.Isp
+                print("m_wet  ", m_wet)
+                print("m_dry  ", m_dry)
+                print("log    ", math.log(m_wet / m_dry))
+                print("log    ", m_wet / m_dry)
+                print("isp    ", s.isp)
             
-            print "dv_temp", dv_temp
+            print("dv_temp", dv_temp)
 
             m_wet -= s.m_wet
 
         return dv
 
-class Stage(object):
-    pass
+class Stage:
+    def __init__(self, m_wet, m_dry, isp):
+        self.m_wet = m_wet
+        self.m_dry = m_dry
+        self.isp = isp
 
+    @property
+    def mass_prop(self):
+        return self.m_wet - self.m_dry
 
 def test1():
     r = Rocket()
-    s1 = Stage()
-    s2 = Stage()
-    
-    s1.Isp = 200.
-    s1.m_wet = 80.0
-    s1.m_dry = 3.0
-    
-    s2.Isp = 200.
-    s2.m_wet = 30.0
-    s2.m_dry = 2.0
+    s1 = Stage(80, 4, 300)
+    s2 = Stage(30, 2, 300)
     
     r.stages = [s1, s2]
     
-    print r.deltav()
+    print('delta v total:', r.deltav())
     
+    return r
+
 def test2():
     r = Rocket()
     s1 = Stage()
@@ -74,13 +80,28 @@ def test2():
     
     r.stages = [s1]
     
-    print r.deltav()
+    print(r.deltav())
+    
+def simulate(r):
+    n = 1001
+    t1 = 100
+    dt = t1 / (n - 1)
+    T = np.linspace(0, t1, n)
+    V = np.zeros(n)
+    
+    stages = list(r.stages)
+    mass = r.mass_wet
+
+    for i in range(1, n):
+        a = thrust / mass
+        V[i] = V[i-1] + a * dt
+    
     
 
 
+r = test1()
 
-test1()
-
+simulate(r)
 
 #radio = Radio()
 #print radio.power(400e3, 5e-6)
